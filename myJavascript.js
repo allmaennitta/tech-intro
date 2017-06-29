6e049b3ad433cdeb42d6769c6ac69b209eae7575
ibeyerlein <ingo.beyerlein@allmaennitta.de>
2017-06-29 03:16:08 +0200

    adding javascript - turn red

--- myJavascript.js
+++ myJavascript.js
@@ -1,1 +1,10 @@
-alert("Halllloooooo!");
+document.body.addEventListener('keydown', toggleRed);
+
+function toggleRed (){
+    if (this.classList.contains("red")){
+        this.classList.remove("red");
+    }
+    else {
+        this.classList.add("red");
+    }
+}
